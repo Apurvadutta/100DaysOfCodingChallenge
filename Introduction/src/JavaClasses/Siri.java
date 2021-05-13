070cd6ac6f3b401e137d2a08305f6e337d7ec8db
Apurva Dutta <apurvadutta2110@gmail.com>
2021-05-13 19:08:59 +0530

    Day 24

---
+++ Introduction/src/JavaClasses/Siri.java
@@ -0,0 +1,16 @@
+package JavaClasses;
+
+class alexa {
+	void add(int a, int b) {
+		System.out.println(a + b);
+	}
+}
+public class Siri {
+
+	public static void main(String[] args) {
+		int x = 5;
+		alexa obj = new alexa();
+		obj.add(10, 8);
+	}
+
+}
